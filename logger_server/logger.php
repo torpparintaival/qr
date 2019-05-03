c4c5ce92b92b2c48f997109eec1dd7615656fa12
Matti Lattu <matti.lattu@iki.fi>
2019-05-03 21:35:48 +0300

    Replace CORS function with hard-coded value, timeout set back to 5 secs

--- logger_server/logger.php
+++ logger_server/logger.php
@@ -1,36 +1,12 @@
 <?php
 
+  header("Access-Control-Allow-Origin: https://torpparintaival.github.io");
+
   include_once("DataBase.php");
   include_once("db_settings.php");
 
   $MAX_LENGTH = 64;
   $RATE_LIMIT_IN_MINUTE = 5;
-
-  function cors() {
-
-      // Allow from any origin
-      if (isset($_SERVER['HTTP_ORIGIN'])) {
-          // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-          // you want to allow, and if so:
-          header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-          header('Access-Control-Max-Age: 86400');    // cache for 1 day
-      }
-
-      // Access-Control headers are received during OPTIONS requests
-      if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-          if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-              // may also be using PUT, PATCH, HEAD etc
-              header("Access-Control-Allow-Methods: POST");
-
-          if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-              header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-          exit(0);
-      }
-  }
-
-  cors();
 
   if (!array_key_exists('data', $_POST)) {
     exit(0);
